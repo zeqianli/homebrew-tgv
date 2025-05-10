bc0c9dfb8c98e4a1adf36f4d11193d78c45910bf
Zeqian Li <zeqianli.bio@gmail.com>
2025-05-09 19:19:06 -0700

    v0.0.5

--- Formula/tgv.rb
+++ Formula/tgv.rb
@@ -4,14 +4,14 @@
 class Tgv < Formula
     desc "Terminal Genome Viewer"
     homepage "https://github.com/zeqianli/tgv/"
-    version "0.0.4"
+    version "0.0.5"
 
     if OS.mac?
         url "https://github.com/zeqianli/tgv/releases/download/v#{version}/tgv-aarch64-apple-darwin.tar.gz"
-        sha256 "9a8167985456cc3893aea80bf703fc8ced31005c8496d7aa6e6ed885ecb1ef33"
+        sha256 "6a0cc406988ca75b1041b32562af43902e24b44d065696168c193b8ea95f226f"
     elsif OS.linux?
         url "https://github.com/zeqianli/tgv/releases/download/v#{version}/tgv-x86_64-linux-musl.tar.gz"
-        sha256 "4c7a434493ce249ed158db3e96dd93b0e1b7ea2bc7f93199def0c134cb00b526"
+        sha256 "033802e6102b418e7fb4ac10abd379a264011899563d6288d6816716a2d6c0e7"
     end
   
     def install
